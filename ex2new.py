fc2232c3614278251c1304c5c6f3d16e865d9149
Matan <melamed.matan1@gmail.com>
2019-05-20 18:54:24 +0300

    new tests

--- ex2new.py
+++ ex2new.py
@@ -2,26 +2,38 @@ import random
 
 import numpy as np
 
+# load and data section
 char_to_bin = {"M": [0., 0., 1.], "F": [0., 1., 0.], "I": [1., 0., 0.]}
 
 
-def load_data(x_file_name, y_file_name):
+class TrainingSet:
+
+    def __init__(self, samples, labels, mins, maxs):
+        self.features_number = len(samples[0])
+        self.classes_number = len(set(labels))
+        self.data = list(zip(samples, labels))
+        self.mins = mins
+        self.maxs = maxs
+
+    def shuffle_data(self):
+        random.shuffle(self.data)
+        return self
+
+
+def load_training_set(x_file_name, y_file_name):
     # load samples
+    mins = []
+    maxs = []
     samples = load_samples(x_file_name)
-    normalize_samples(samples)
+    normalize_samples(samples, mins, maxs)
 
     # load labels
     labels = load_labels(y_file_name)
 
     # shuffle and return as set of data
-    samples, labels = shuffle_set(samples, labels)
-    return samples, labels
+    training_set = TrainingSet(samples, labels, mins, maxs)
 
-
-def load_test_samples(samples_file_name):
-    samples = load_samples(samples_file_name)
-    normalize_samples(samples)
-    return samples
+    return training_set.shuffle_data()
 
 
 def load_samples(file_name):
@@ -34,11 +46,6 @@ def load_samples(file_name):
         data.append(ts)
     file.close()
     return np.asarray(data)
-
-
-def load_labels(file_name):
-    file = open(file_name)
-    return np.asarray([int(float(line.split()[0])) for line in file])
 
 
 def zscore_normalize(samples_to_normalize):
@@ -59,38 +66,51 @@ def zscore_normalize(samples_to_normalize):
                 row[i] = (row[i] - means[i]) / deviations[i]
 
 
-def minmax_normalize(samples_to_normalize):
+def minmax_normalize(samples_to_normalize, mins, maxs):
     columns_number = len(samples_to_normalize[0])
 
-    samples_transpose = samples_to_normalize.T
-    maxs = np.empty([columns_number])
-    mins = np.empty([columns_number])
-    for i in range(columns_number):
-        column = np.asarray(samples_transpose[i])
-        maxs[i] = column.max()
-        mins[i] = column.min()
+    if len(mins) == 0:
+        samples_transpose = samples_to_normalize.T
+        mins = np.empty([columns_number])
+        maxs = np.empty([columns_number])
+        for i in range(columns_number):
+            column = np.asarray(samples_transpose[i])
+            maxs[i] = column.max()
+            mins[i] = column.min()
 
     for row in samples_to_normalize:
         for i in range(columns_number):
-
             if (maxs[i] - mins[i]) != 0:
                 row[i] = (row[i] - mins[i]) / (maxs[i] - mins[i])
 
 
-def normalize_samples(samples_to_normalize):
+def normalize_samples(samples_to_normalize, mins=None, maxs=None):
     # zscore_normalize(samples_to_normalize)
-    minmax_normalize(samples_to_normalize)
+    minmax_normalize(samples_to_normalize, mins, maxs)
 
 
-def shuffle_set(samples, labels):
-    shuffled_set = list(zip(samples, labels))
-    random.shuffle(shuffled_set)
-    return zip(*shuffled_set)
+def load_labels(file_name):
+    file = open(file_name)
+    return np.asarray([int(float(line.split()[0])) for line in file])
 
+
+def load_test_samples(samples_file_name, mins=None, maxs=None):
+    if mins is None:
+        mins = []
+    if maxs is None:
+        maxs = []
+    samples = load_samples(samples_file_name)
+    normalize_samples(samples, mins, maxs)
+
+    return samples
+
+
+# train and predict  section
 
 def train_perceptron(data, eta, epochs, w):
     for e in range(epochs):
         random.shuffle(data)
+        eta = np.power(eta, 2)
 
         for x, y in data:
             y_hat = np.argmax(np.dot(w, x))
@@ -104,6 +124,7 @@ def train_perceptron(data, eta, epochs, w):
 def train_svm(data, eta, epochs, w, gradient):
     for e in range(epochs):
         random.shuffle(data)
+        eta = np.power(eta, 2)
 
         for x, y in data:
             y_hat = np.argmax(np.dot(w, x))
@@ -123,11 +144,10 @@ def train_pa(data, epochs, w):
 
         for x, y in data:
             y_hat = np.argmax(np.dot(w, x))
-            if y_hat != y:
-                loss = 1 - np.dot(w[y], x) + np.dot(w[y_hat], x)
-                tau = loss / (2 * (np.power((np.linalg.norm(x)), 2)))
-                w[y] = w[y] + (tau * x)
-                w[y_hat] = w[y_hat] - (tau * x)
+            loss = max(0, 1 - np.dot(w[y], x) + np.dot(w[y_hat], x))
+            tau = loss / (2 * (np.power((np.linalg.norm(x)), 2)))
+            w[y] = w[y] + (tau * x)
+            w[y_hat] = w[y_hat] - (tau * x)
 
     return w
 
@@ -142,13 +162,7 @@ def predict_using_hypothesis(hypothesis, samples):
     return np.asarray(predictions)
 
 
-class Result:
-
-    def __init__(self, eta, epochs, error):
-        self.eta = eta
-        self.epochs = epochs
-        self.error = error
-
+# test section
 
 def split_to_equal_arrays(data, parts_num):
     size_of_each_array = len(data) / float(parts_num)
@@ -173,99 +187,73 @@ def test_hypothesis_on_data(hypothesis, data):
     return (errors / len(data)) * 100
 
 
-def cross_val_perceptron(k_parts, eta, epochs):
-    # load and divide data
-    samples, labels = load_data("train_x.txt", "train_y.txt")
-    data = list(zip(samples, labels))
-    arrays = split_to_equal_arrays(data, k_parts)
+def cross_val_perceptron(arrays, eta, epoch, hypothesis):
+    k_parts = len(arrays)
 
-    # create initial hypothesis
-    features_number = len(samples[0])
-    classes_number = len(set(labels))
-    hypothesis = np.zeros((classes_number, features_number))
-
-    # train and validate using cross validation with k parts
-    errors = 0
+    error = 0
     for i in range(k_parts):
         for j in (value for value in range(k_parts) if value != i):
-            hypothesis = train_perceptron(arrays[j], eta, epochs, hypothesis)
-        errors += test_hypothesis_on_data(hypothesis, arrays[i])
+            hypothesis = train_perceptron(arrays[j], eta, epoch, hypothesis)
+        error += test_hypothesis_on_data(hypothesis, arrays[i])
 
-    return errors / k_parts
+    return error / k_parts
 
 
-def cross_val_svm(k_parts, eta, epochs, gradient):
-    # load and divide data
-    samples, labels = load_data("train_x.txt", "train_y.txt")
-    data = list(zip(samples, labels))
-    arrays = split_to_equal_arrays(data, k_parts)
+def cross_val_svm(arrays, eta, epoch, hypothesis, gradient):
+    k_parts = len(arrays)
 
-    # create initial hypothesis
-    features_number = len(samples[0])
-    classes_number = len(set(labels))
-    hypothesis = np.zeros((classes_number, features_number))
-
-    # train and validate using cross validation with k parts
-    errors = 0
+    error = 0
     for i in range(k_parts):
         for j in (value for value in range(k_parts) if value != i):
-            hypothesis = train_svm(arrays[j], eta, epochs, hypothesis, gradient)
-        errors += test_hypothesis_on_data(hypothesis, arrays[i])
+            hypothesis = train_svm(arrays[j], eta, epoch, hypothesis, gradient)
+        error += test_hypothesis_on_data(hypothesis, arrays[i])
 
-    return errors / k_parts
-
-
-def cross_val_pa(k_parts, epochs):
-    # load and divide data
-    samples, labels = load_data("train_x.txt", "train_y.txt")
-    data = list(zip(samples, labels))
-    arrays = split_to_equal_arrays(data, k_parts)
-
-    # create initial hypothesis
-    features_number = len(samples[0])
-    classes_number = len(set(labels))
-    hypothesis = np.zeros((classes_number, features_number))
-
-    # train and validate using cross validation with k parts
-    errors = 0
-    for i in range(k_parts):
-        for j in (value for value in range(k_parts) if value != i):
-            hypothesis = train_pa(arrays[j], epochs, hypothesis)
-        errors += test_hypothesis_on_data(hypothesis, arrays[i])
-
-    return errors / k_parts
+    return error / k_parts
 
 
-def test_cross_val_with_params():
-    random.seed(30)
+def get_tests_results(arrays, training_set, eta):
+    x_values = []
     errors = []
 
-    for eta in range(1, 10):
-        eta = eta / 100
-        print("checking eta ", eta, " with epoch: ", end=" ")
-        for epoch in range(10, 16):
-            print(epoch, end=" - ")
-            error = cross_val_perceptron(5, eta, epoch)
-            print(str(error)[:5], end="%, ")
-            errors.append(Result(eta, epoch, error))
-        print()
+    for epoch in range(1, 23):
+        epoch *= 5
+        print(epoch, end=" ")
+        hypothesis = np.zeros((training_set.classes_number, training_set.features_number))
+        error = cross_val_svm(arrays, eta, epoch, hypothesis, 0.1)
 
-    from operator import attrgetter
-    errors.sort(key=attrgetter('error'))
-    for i in range(min(len(errors), 10)):
-        print(
-            "eta: " + str(errors[i].eta) + " epochs: " + str(errors[i].epochs) + " error: " + str(errors[i].error))
+        x_values.append(epoch)
+        errors.append(error)
+
+    return x_values, errors
+
+
+def tests():
+    # load and divide data
+    training_set = load_training_set("train_x.txt", "train_y.txt")
+    arrays = split_to_equal_arrays(training_set.data, 5)
+
+    import matplotlib.pyplot as plt
+    import time
+    check_time = True
+    start = None
+
+    for eta in range(1, 10):
+        if check_time:
+            start = time.time()
+
+        eta /= 10
+        print("checking eta {}".format(eta), end=": doing epoch ")
+        p, e = get_tests_results(arrays, training_set, eta)
+        plt.plot(p, e, label="eta {}".format(eta))
+        if check_time:
+            end = time.time()
+            check_time = False
+            print()
+            print("each eta take {}".format(end - start), end="")
+        print()
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
-    # generate_ts_and_test_from_files("train_x.txt", "train_y.txt")
-    # single_run()
-    # test_cross_val_with_params()
-    random.seed(30)
-    print(cross_val_perceptron(5, 0.07, 19))
-
-    # arr = split_to_equal_arrays(data, 3)
-    # s, l = zip(*arr[0])
-    # print(data[0])
-    # pa_tests()
-    # tests()
+    tests()
